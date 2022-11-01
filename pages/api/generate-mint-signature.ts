62ccbcbe2cf59cd1a6d0983e890ead4321ddda3a
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-11-01 20:33:29 +0100

    Add more error handling

--- pages/api/generate-mint-signature.ts
+++ pages/api/generate-mint-signature.ts
@@ -11,16 +11,20 @@ async function accessSecretVersion() {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
     },
   });
-  const [version] = await client.accessSecretVersion({ name });
-
-  // Extract the payload as a string.
-  const payload = version.payload?.data?.toString();
-
-  // WARNING: Do not print the secret in a production environment - this
-  // snippet is showing how to access the secret material.
-  //console.info(`Payload: ${payload}`);
-
-  return payload;
+  try {
+    const [version] = await client.accessSecretVersion({ name });
+  
+    // Extract the payload as a string.
+    const payload = version.payload?.data?.toString();
+  
+    // WARNING: Do not print the secret in a production environment - this
+    // snippet is showing how to access the secret material.
+    //console.info(`Payload: ${payload}`);
+  
+    return payload;
+  } catch (error) {
+    console.error(`Theres's been an error accessing the secret version: ${error}`);
+  }
 }
 
 export default async function generateMintSignature(
@@ -31,6 +35,25 @@ export default async function generateMintSignature(
   const { address, quantity } = JSON.parse(req.body);
 
   try {
+    const PRIVATE_KEY = await accessSecretVersion();
+
+    if (!PRIVATE_KEY) {
+      console.error('Missing ADMIN_PRIVATE_KEY environment variable');
+      return res.status(500).json({
+        error: 'Admin private key not set',
+      });
+    }
+
+    const binanceSmartChainMainnetSDK = ThirdwebSDK.fromPrivateKey(
+      PRIVATE_KEY.toString(),
+      'binance',
+    );
+
+    const signatureDrop = await binanceSmartChainMainnetSDK.getContract(
+      '0x1615600fE62ed38342F82eb9785029A2b1290DAF',
+      'signature-drop',
+    );
+
     // Get the Early Access NFT Drop contract
     const polygonSDK = new ThirdwebSDK('polygon'); // change to real chain of wolfer NFT
     const earlyAccessNfts = await polygonSDK.getContract(
@@ -45,43 +68,26 @@ export default async function generateMintSignature(
       userHasToken = true;
     }
 
-    const PRIVATE_KEY = await accessSecretVersion();
-
-    if (!PRIVATE_KEY) {
-      console.error('Missing ADMIN_PRIVATE_KEY environment variable');
-      return res.status(500).json({
-        error: 'Admin private key not set',
-      });
-    }
-
-    const BinanceSmartChainMainnetSDK = ThirdwebSDK.fromPrivateKey(
-      PRIVATE_KEY.toString(),
-      'binance',
-    );
-
-    const signatureDrop = await BinanceSmartChainMainnetSDK.getContract(
-      '0x1615600fE62ed38342F82eb9785029A2b1290DAF',
-      'signature-drop',
-    );
-
-    const price = quantity >= 3 ? "200" : "225";
+    const price = parseInt(quantity) >= 3 ? '200' : '225';
 
     // If the user has an early access NFT, generate a mint signature
     if (userHasToken) {
+      console.log("User has token!")
       const mintSignature = await signatureDrop?.signature.generate({
         to: address, // Can only be minted by the address we checked earlier
-        quantity: quantity,
-        price: parseInt(quantity) < 3 ? '225' : '200',
-        currencyAddress: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
-        mintStartTime: new Date(0), // now
+        quantity,
+        price,
+        currencyAddress: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', // BUSD
       });
       res.status(200).json(mintSignature);
     } else {
+      console.log("User doesn't have token!")
       res.status(400).json({
         message: 'User does not have an early access Wolfer NFT',
       });
     }
   } catch (error) {
+    console.log("Something went wrong.")
     console.error(error);
     res.status(500).json({
       error: (error as Error).message,
